74f8494dd7ae1a7d3a5b607d6eabb2b2c0aa7e76
Toni Möckel <toni.moeckel@dreebit.com>
2019-02-12 12:56:55 +0100

    Fix HTML String

--- src/root/index.ts
+++ src/root/index.ts
@@ -2,11 +2,22 @@ import * as fs from "fs";
 import * as path from "path";
 import db from "./../persists";
 
+const htmlString = "<!DOCTYPE html>\n" +
+    "<html lang=\"en\">\n" +
+    "<head>\n" +
+    "    <meta charset=\"UTF-8\">\n" +
+    "    <title>Prisma Pro API Simulation</title>\n" +
+    "</head>\n" +
+    "<body>\n" +
+    "<h1>Prisma API Simulation</h1>\n" +
+    "<ul>\n" +
+    "    {{paths}}\n" +
+    "</ul>\n" +
+    "</body>\n" +
+    "</html>\n";
+
 export default function(req, res) {
-    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
-        let htmlString = data.toString();
-        const paths = Object.keys(db.get("paths").value()).map((pathItem) => `<li><a href="${pathItem}">${pathItem}</a></li>`);
-        htmlString = htmlString.replace("{{paths}}", paths.join(""));
-        res.send(htmlString);
-    });
+    const paths = Object.keys(db.get("paths").value()).map((pathItem) => `<li><a href="${pathItem}">${pathItem}</a></li>`);
+    const render = htmlString.replace("{{paths}}", paths.join(""));
+    res.send(render);
 }
