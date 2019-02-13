7769e72d599bb4483199eaa6afc7e36d2437f494
Toni Möckel <toni.moeckel@dreebit.com>
2019-02-13 09:55:21 +0100

    Paging

--- src/dynamics/getMeasurementData.ts
+++ src/dynamics/getMeasurementData.ts
@@ -1,9 +1,24 @@
+import * as _ from "lodash";
 import {IRequestOptions} from "../types";
 import db from "./../persists";
 
 
 export default ({urlParams, bodyParams, qsParams, path}: IRequestOptions) => {
 
-    return db.get("measurementData").value();
+    const start = parseInt(_.get(qsParams, "@start"), undefined) || 0;
+    const end = parseInt(_.get(qsParams, "@end"), undefined) || -1;
+
+    const measurement = db.get("measurementData").value();
+    let values = _.chain(measurement).get("data.values",[]).value();
+
+    if (start) {
+        values = _.drop(values, start);
+    }
+    if (end) {
+        values = _.take(values, end - start);
+    }
+    _.set(measurement, "data.values", values);
+
+    return measurement;
 
 };
